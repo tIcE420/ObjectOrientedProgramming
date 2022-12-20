ccebbd361a7eff9027ee4cdb395a36f98dc1d679
dude <miro.miraf@gmail.com>
2022-12-21 00:45:37 +0100

    commit

--- Vjezba_6/funk.cpp
+++ Vjezba_6/funk.cpp
@@ -1,20 +1,20 @@
 #include "main_head.hpp"
 
-std::vector<Zivotinja*> make_land_octopuses(std::vector<Zivotinja*> animal_kingdom) {
-	std::cout << "Enter how many nopes do you want?";
+std::vector<Zivotinja*> make_spiders(std::vector<Zivotinja*> animal_kingdom) {
+	std::cout << "Enter how many spiders do you want?";
 	int a = 0;
 	std::cin >> a;
 	
 	std::string name;
 	for (int i = 0; i < a; i++) {
 		Zivotinja* many_legged_nope;
-		std::cout << "Enter the name of scary nope num: "<<i + 1<<" ";
+		std::cout << "Enter the name or species of the spider: "<<i + 1<<" ";
 		std::cin >> name;
 		if (name == "Tarantula" || name == "tarantula")
 		{
-			std::cout << "Enter the name of your tarantino_nope" << std::endl;;
+			std::cout << "Enter the name of your tarantula" << std::endl;;
 			std::cin >> name;
-			animal_kingdom = make_tarantinotula(animal_kingdom,name);
+			animal_kingdom = make_tarantulas(animal_kingdom,name);
 		}
 		else {
 			Pauk* novi = new Pauk();
@@ -37,12 +37,12 @@ std::vector<Zivotinja*> make_birbs(std::vector<Zivotinja*> animal_kingdom) {
 	for (int i = 0; i < a; i++) {
 	
 		Zivotinja* birb;
-		std::cout << "Enter the birb name num: " << i + 1 << " ";
+		std::cout << "Enter the name or species of the bird num: " << i + 1 << " ";
 		std::cin >> name;
 		if (name == "vrabac" || name == "Vrabac" || name == "Sparrow" || name == "sparrow") {
 			std::cout << "Enter the name of your Spparow: " << std::endl;
 			std::cin >> name;
-			animal_kingdom = make_vrabac(animal_kingdom, name);
+			animal_kingdom = make_sparrow(animal_kingdom, name);
 		}
 		else {
 			Ptica* novi = new Ptica();
@@ -55,15 +55,15 @@ std::vector<Zivotinja*> make_birbs(std::vector<Zivotinja*> animal_kingdom) {
 	return animal_kingdom;
 
 }
-std::vector<Zivotinja*> make_milk_lovers(std::vector<Zivotinja*> animal_kingdom) {
-	std::cout << "Enter how many milkers do you want?";
+std::vector<Zivotinja*> make_mammals(std::vector<Zivotinja*> animal_kingdom) {
+	std::cout << "Enter how many mammals do you want?";
 	int a = 0;
 	std::cin >> a;
 	std::string name;
 	for (int i = 0; i < a; i++) {
 
 		Zivotinja* milker;
-		std::cout << "Enter the name of milker num: " << i + 1 << " ";
+		std::cout << "Enter the name or species of the mammal num: " << i + 1 << " ";
 		std::cin >> name;
 		Sisavac* novi = new Sisavac();
 		novi->change_species(name);
@@ -73,15 +73,15 @@ std::vector<Zivotinja*> make_milk_lovers(std::vector<Zivotinja*> animal_kingdom)
 
 	return animal_kingdom;
 }
-std::vector<Zivotinja*> make_buzzers(std::vector<Zivotinja*> animal_kingdom) {
-	std::cout << "Enter how many buzzers do you want?";
+std::vector<Zivotinja*> make_insects(std::vector<Zivotinja*> animal_kingdom) {
+	std::cout << "Enter how many insects do you want?";
 	int a = 0;
 	std::cin >> a;
 	std::string name;
 	for (int i = 0; i < a; i++) {
 
 		Zivotinja* buzzer;
-		std::cout << "Enter the name of needle buzz poker num: " << i + 1 << " ";
+		std::cout << "Enter the name or species of insect num: " << i + 1 << " ";
 		std::cin >> name;
 		if (name == "Zohar" || name == "zohar" || name == "cockroach" || name == "roach" || name == "Cockroach" || name == "Roach") {
 			animal_kingdom = make_roach(animal_kingdom,name);
@@ -96,7 +96,7 @@ std::vector<Zivotinja*> make_buzzers(std::vector<Zivotinja*> animal_kingdom) {
 
 	return animal_kingdom;
 }
-std::vector<Zivotinja*> make_tarantinotula(std::vector<Zivotinja*> animal_kingdom,std::string name) {
+std::vector<Zivotinja*> make_tarantulas(std::vector<Zivotinja*> animal_kingdom,std::string name) {
 	Tarantula* Mirko = new Tarantula();
 	Mirko->change_name(name);
 	Zivotinja* tarantino;
@@ -106,7 +106,7 @@ std::vector<Zivotinja*> make_tarantinotula(std::vector<Zivotinja*> animal_kingdo
 	return animal_kingdom;
 	
 }
-std::vector<Zivotinja*> make_vrabac(std::vector<Zivotinja*> animal_kingdom, std::string name) {
+std::vector<Zivotinja*> make_sparrow(std::vector<Zivotinja*> animal_kingdom, std::string name) {
 	Vrabac* Mirko = new Vrabac();
 	Mirko->change_name(name);
 	Zivotinja* birb ;
@@ -139,7 +139,7 @@ std::vector<Zivotinja*>make_any(){
 	std::vector<Zivotinja*> animal_kingdom;
 	while (true) {
 		if (choice == "Spider") {
-			animal_kingdom = make_land_octopuses(animal_kingdom);
+			animal_kingdom = make_spiders(animal_kingdom);
 			std::cout << "Do you want to make any more if yes just type in your choice"<<std::endl;
 			std::cout << "If not just type 1";
 			std::cin >> choice;
@@ -151,13 +151,13 @@ std::vector<Zivotinja*>make_any(){
 			std::cin >> choice;
 		}
 		else if (choice == "Mammal") {
-			animal_kingdom = make_milk_lovers(animal_kingdom);
+			animal_kingdom = make_mammals(animal_kingdom);
 			std::cout << "Do you want to make any more if yes just type in your choice" << std::endl;
 			std::cout << "If not just type 1";
 			std::cin >> choice;
 		}
 		else if (choice == "Insect") {
-			animal_kingdom = make_buzzers(animal_kingdom);
+			animal_kingdom = make_insects(animal_kingdom);
 			std::cout << "Do you want to make any more if yes just type in your choice" << std::endl;
 			std::cout << "If not just type 1";
 			std::cin >> choice;
--- Vjezba_6/main_head.hpp
+++ Vjezba_6/main_head.hpp
@@ -9,11 +9,11 @@
 #include "Brojac.hpp"
 #include <vector>
 std::vector<Zivotinja*>make_any();
-std::vector<Zivotinja*> make_land_octopuses(std::vector<Zivotinja*> animal_kingdom);
+std::vector<Zivotinja*> make_spiders(std::vector<Zivotinja*> animal_kingdom);
 std::vector<Zivotinja*> make_birbs(std::vector<Zivotinja*> animal_kingdom);
-std::vector<Zivotinja*> make_milk_lovers(std::vector<Zivotinja*> animal_kingdom);
-std::vector<Zivotinja*> make_buzzers(std::vector<Zivotinja*> animal_kingdom);
-std::vector<Zivotinja*> make_tarantinotula(std::vector<Zivotinja*> animal_kingdom,std::string name);
-std::vector<Zivotinja*> make_vrabac(std::vector<Zivotinja*> animal_kingdom, std::string name);
+std::vector<Zivotinja*> make_mammals(std::vector<Zivotinja*> animal_kingdom);
+std::vector<Zivotinja*> make_insects(std::vector<Zivotinja*> animal_kingdom);
+std::vector<Zivotinja*> make_tarantulas(std::vector<Zivotinja*> animal_kingdom,std::string name);
+std::vector<Zivotinja*> make_sparrow(std::vector<Zivotinja*> animal_kingdom, std::string name);
 std::vector<Zivotinja*> make_roach(std::vector<Zivotinja*> animal_kingdom, std::string name);
 void destroy_vect(std::vector<Zivotinja*> animal_kingdom);
